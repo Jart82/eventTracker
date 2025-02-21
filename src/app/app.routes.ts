c141053050d4247bae73002fdb4d555eff1d06be
jart82 <agarin3@gmail.com>
2025-02-21 22:28:01 +0100

    removing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
-export const routes: Routes = [
-    {path: '', component: AppComponent}
-];
+
+export const routes: Routes = [];
 
 export class AppRoutingModule {}
